29c42812738d419842b6c4c4052e0200ad3561c7
Olavur Ellefsen <oe@tokni.com>
2019-07-16 07:29:29 +0200

    Change props from array to object

--- src/RenderCharts.js
+++ src/RenderCharts.js
@@ -1,8 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
 import { Flex } from 'RenderCharts.style'
 import RenderChart from './RenderChart'
 
-export default ({ config }) => (
-  <Flex>
-    {config.map((chartConfig,i) => <RenderChart key={i} config={chartConfig} />)}    
-  </Flex>
-)
+const RenderCharts = ({ config }) => {
+  const chartConfigs = Object.values(config)
+  return (
+    <Flex>
+      {chartConfigs.map((chartConfig, i) => (
+        <RenderChart key={i} config={chartConfig} />
+      ))}
+    </Flex>
+  )
+}
+
+RenderCharts.propTypes = {
+  config: PropTypes.object.isRequired,
+}
+
+export default RenderCharts
