7ede59362ab0e0b6b4b4e51c83779affa95195e1
Olavur Ellefsen <oe@tokni.com>
2019-07-16 06:12:06 +0200

    Fix flashy render of multicharts

--- src/RenderCharts.js
+++ src/RenderCharts.js
@@ -1,36 +1,8 @@
-import RenderBarChart from './charts/RenderBarChart'
-import RenderPieChart from './charts/RenderPieChart'
-import { connect } from 'react-redux'
-import { ChartWrapper, ChartTitle } from 'RenderCharts.style'
+import { Flex } from 'RenderCharts.style'
+import RenderChart from './RenderChart'
 
-const RenderCharts = ({ config, pathways }) => {
-  let result
-  switch (config.chartType) {
-    case 'BarChart':
-      result = (
-        <RenderBarChart
-          data={config.data}
-          bars={config.bars.filter(bar => pathways.indexOf(bar.dataKey) > -1)}
-          yAxisLabel={config.yAxisLabel}
-        />
-      )
-      break
-    case 'PieChart':
-      result = <RenderPieChart data={config.data} />
-      break
-    default:
-  }
-  return (
-    <ChartWrapper>
-      <ChartTitle>{config.title}</ChartTitle>
-      {result}
-    </ChartWrapper>
-  )
-}
-
-export default connect(
-  state => ({
-    pathways: state.app.pathways,
-  }),
-  null
-)(RenderCharts)
+export default ({ config }) => (
+  <Flex>
+    {config.map((chartConfig,i) => <RenderChart key={i} config={chartConfig} />)}    
+  </Flex>
+)
